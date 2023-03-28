9382c5a1d90232a163e3fabae356c0ce826adae7
Yash James <jamesyash007@gmail.com>
2023-03-28 19:22:22 +0530

    Design - 2 (Updated Condition)

--- app.js
+++ app.js
@@ -9,19 +9,23 @@ let z ;
 document.getElementById("myBtn").onclick = function() {
 
      // Generate Number Here ;
-     n = Math.floor(Math.random() * 3) + 1;
-     y = Math.ceil(Math.random() * 3) + 1;
+     n = Math.floor((Math.random() * 6) + 1);
+     y = Math.floor((Math.random() * 6) + 1);
 
      // Show the Number in HTML Page :
      document.getElementById("text1").innerHTML = " Your first Number is : " + n ;
      document.getElementById("text2").innerHTML = " Your Second Number is : " + y ;
-
-     // IF Conditional :
-     if(n === y) {
-          z = "true" ;
+     
+     // if Conditional for 6 :
+     
+     if(n === 6 && y === 6){
+          z = "You Got 6" ;
+          document.getElementById("text3").innerHTML = z ;
+     }else if(n === y){
+          z = "True";
           document.getElementById("text3").innerHTML = z ;
      }else {
-          z = "False" ;
+          z = "False";
           document.getElementById("text3").innerHTML = z ;
      }
 
