70fd2a92aebb6bc9784116815f2cafe44fdc51a3
Yash James <jamesyash007@gmail.com>
2023-03-28 23:49:46 +0530

    my design - 3

--- app.js
+++ app.js
@@ -1,34 +1,14 @@
-// Random NUmber Generator :
-
-let n ;
-let y ;
-let z ; 
-
-// Main-Thing :
+/// Some Few More Conditional code :
+let x;
 
 document.getElementById("myBtn").onclick = function() {
-
-     // Generate Number Here ;
-     n = Math.floor((Math.random() * 6) + 1);
-     y = Math.floor((Math.random() * 6) + 1);
-
-     // Show the Number in HTML Page :
-     document.getElementById("text1").innerHTML = " Your first Number is : " + n ;
-     document.getElementById("text2").innerHTML = " Your Second Number is : " + y ;
-     
-     // if Conditional for 6 :
-     
-     if(n === 6 && y === 6){
-          z = "You Got 6" ;
-          document.getElementById("text3").innerHTML = z ;
-     }else if(n === y){
-          z = "True";
-          document.getElementById("text3").innerHTML = z ;
+     if(document.getElementById('check1').checked){
+          x = " The BOX is Checked !";
+          document.getElementById("myText").innerHTML = x ;
      }else {
-          z = "False";
-          document.getElementById("text3").innerHTML = z ;
+          x = " the BOX is Not Checked !";
+          document.getElementById("myText").innerHTML = x ;
      }
-
-
 }
 
+console.log(x);
